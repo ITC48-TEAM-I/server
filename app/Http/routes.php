9057d8516a865350834feff559ada383d4d6f316
44x1carbon <b4079@oic.jp>
2016-12-12 09:58:26 +0900

    ルーティング記述

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -10,7 +10,42 @@
 | and give it the controller to call when that URI is requested.
 |
 */
+/*ユーザー登録*/
+Route::post('/api/users', function() {
 
-Route::get('/', function () {
-    return view('welcome');
+});
+
+/*ユーザー認証*/
+Route::post('/api/auth', function() {
+
+});
+
+/*旅行登録*/
+Route::post('/api/users/{ user_code }/travels', function() {
+
+});
+
+/*旅行一覧取得*/
+Route::get('/api/user{ user_code }/travels', function() {
+
+});
+
+/*旅行取得*/
+Route::get('/api/travels/{ travel_code }', function() {
+
+});
+
+/*道順登録*/
+Route::post('/api/travels/{ travel_code }/routes', function() {
+
+});
+
+/*立ち寄った場所登録*/
+Route::post('/api/travels/{ travel_code }/spots', function() {
+
+});
+
+/*立ち寄った場所更新*/
+Route::put('/api/spots/{ spot_code }'. function() {
+
 });
