c1f143a01d34a9ae3ef2c5995f8b7661c256498f
ReiOogami <b4049@oic.jp>
2016-12-13 23:40:03 +0900

    make Models and Relation

---
+++ app/Route.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Route extends Model
+{
+    protected $table = 'route';
+
+    protected $fillable = [
+        'travel_id','transit_time','latitude','longitude',
+    ];
+
+    public function Travel()
+    {
+        return $this->belongsToMany('App\Travel','travel_id');
+    }
+}
