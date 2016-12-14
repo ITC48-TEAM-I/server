cc5eb2fc2f46bfcb531ef4504b8f973ee101c8ab
44x1carbon <b4079@oic.jp>
2016-12-14 19:21:23 +0900

    カテゴリーコード追加

--- app/User.php
+++ app/User.php
@@ -19,7 +19,7 @@ class User extends Authenticatable
 
     public function travels()
     {
-        return $this->belongsToMany('App\Travel','user_id');
+        return $this->hasMany('App\Travel','user_id');
     }
 
     public function scopeFindCode($query,$code)
