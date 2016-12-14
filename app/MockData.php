76f0ca028f8f6228499cb5572232e8461a0bb47a
44x1carbon <b4079@oic.jp>
2016-12-15 01:04:55 +0900

    京都のモックデータ

--- app/MockData.php
+++ app/MockData.php
@@ -8,74 +8,235 @@ class MockData {
 		$this->travelList  = [
 			[
 				"travel_code" => "abcdefg",
-				"travel_date" => "2016-12-12",
-				"latitude" => 35.665721,
-				"longitude" => 139.731006,
-				"area_name" => "六本木",
+				"travel_date" => "2016-11-26",
+				"latitude" => 35.010183003031216,
+				"longitude" => 135.6813364562972,
+				"area_name" => "嵐山",
 				"country_name" => "日本",
 				"routes" => [
 					[
-						"transit_time" => "10:13:00",
-						"latitude" => 35.665021590968486,
-						"longitude" => 139.73028116798397
+						"latitude" => 35.010183003031216,
+						"longitude" => 135.6813364562972,
+						"transit_time" => "00 :00 :00"
 					],
 					[
-						"transit_time" => "10:13:15",
-						"latitude" => 35.66442019710309,
-						"longitude" => 139.73110252421
+						"latitude" => 35.01037632679415,
+						"longitude" => 135.68106823539568,
+						"transit_time" => "00 :00 :00"
 					],
 					[
-						"transit_time" => "10:13:30",
-						"latitude" => 35.664919626970736,
-						"longitude" => 139.7317355594639
+						"latitude" => 35.010806909896736,
+						"longitude" => 135.6810467777235,
+						"transit_time" => "00 :00 :00"
 					],
 					[
-						"transit_time" => "10:13:45",
-						"latitude" => 35.66535426709936, 
-						"longitude" => 139.73244000538708
+						"latitude" => 35.011334151425054,
+						"longitude" => 135.680585437773,
+						"transit_time" => "00 :00 :00"
 					],
 					[
-						"transit_time" => "10:14:00",
-						"latitude" => 35.6669114989883,
-						"longitude" => 139.73354293665648
+						"latitude" => 35.011422024682695,
+						"longitude" => 135.67965202903582,
+						"transit_time" => "00 :00 :00"
 					],
 					[
-						"transit_time" => "10:15:15",
-						"latitude" => 35.66746056736638,
-						"longitude" => 139.7329722145452
+						"latitude" => 35.01126000829051,
+						"longitude" => 135.67885809516747,
+						"transit_time" => "00 :00 :00"
 					],
 					[
-						"transit_time" => "10:16:15",
-						"latitude" => 35.667784384057526, 
-						"longitude" => 139.73316392712337
+						"latitude" => 35.01107108029564,
+						"longitude" => 135.6781714496595,
+						"transit_time" => "00 :00 :00"
 					],
 					[
-						"transit_time" => "10:17:15",
-						"latitude" => 35.668859752941586, 
-						"longitude" => 139.73236638250415
+						"latitude" => 35.01095245086653,
+						"longitude" => 135.67783349132364,
+						"transit_time" => "00 :00 :00"
 					],
-					
+					[
+						"latitude" => 35.0113215196368,
+						"longitude" => 135.677361422537,
+						"transit_time" => "00 :00 :00"
+					],
+					[
+						"latitude" => 35.0115104470532,
+						"longitude" => 135.67740970229926,
+						"transit_time" => "00 :00 :00"
+					],
+					[
+						"latitude" => 35.01196299234569,
+						"longitude" => 135.67761891460245,
+						"transit_time" => "00 :00 :00"
+					],
+					[
+						"latitude" => 35.013663310047384,
+						"longitude" => 135.67785494899584,
+						"transit_time" => "00 :00 :00"
+					],
+					[
+						"latitude" => 35.01507170212461,
+						"longitude" => 135.67737215137342,
+						"transit_time" => "00 :00 :00"
+					],
+					[
+						"latitude" => 35.01504094786765,
+						"longitude" => 135.67705833291794,
+						"transit_time" => "00 :00 :00"
+					],
+					[
+						"latitude" => 35.01519471903702,
+						"longitude" => 135.6773265538197,
+						"transit_time" => "00 :00 :00"
+					],
+					[
+						"latitude" => 35.01519471903704,
+						"longitude" => 135.67708247279901,
+						"transit_time" => "00 :00 :00"
+					],
+					[
+						"latitude" => 35.01557255553953,
+						"longitude" => 135.67693495130317,
+						"transit_time" => "00 :00 :00"
+					],
+					[
+						"latitude" => 35.01542729738135,
+						"longitude" => 135.6763234076478,
+						"transit_time" => "00 :00 :00"
+					],
+					[
+						"latitude" => 35.01537896946813,
+						"longitude" => 135.67558848237775,
+						"transit_time" => "00 :00 :00"
+					],
+					[
+						"latitude" => 35.01561621531353,
+						"longitude" => 135.67556702470563,
+						"transit_time" => "00 :00 :00"
+					],
+					[
+						"latitude" => 35.01563378905248,
+						"longitude" => 135.67578696584482,
+						"transit_time" => "00 :00 :00"
+					],
+					[
+						"latitude" => 35.01561182187821,
+						"longitude" => 135.67552947377936,
+						"transit_time" => "00 :00 :00"
+					],
+					[
+						"latitude" => 35.015374576020065,
+						"longitude" => 135.67555093145154,
+						"transit_time" => "00 :00 :00"
+					],
+					[
+						"latitude" => 35.01539214981095,
+						"longitude" => 135.67461215829664,
+						"transit_time" => "00 :00 :00"
+					],
+					[
+						"latitude" => 35.01513293601266,
+						"longitude" => 135.67458533620658,
+						"transit_time" => "00 :00 :00"
+					],
+					[
+						"latitude" => 35.01496598431768,
+						"longitude" => 135.67431711530503,
+						"transit_time" => "00 :00 :00"
+					],
+					[
+						"latitude" => 35.01415318830278,
+						"longitude" => 135.6742044625268,
+						"transit_time" => "00 :00 :00"
+					],
+					[
+						"latitude" => 35.014034563344,
+						"longitude" => 135.67387723302704,
+						"transit_time" => "00 :00 :00"
+					],
+					[
+						"latitude" => 35.01428060010387,
+						"longitude" => 135.67350708818293,
+						"transit_time" => "00 :00 :00"
+					],
+					[
+						"latitude" => 35.014456340193476,
+						"longitude" => 135.67342125749445,
+						"transit_time" => "00 :00 :00"
+					],
+					[
+						"latitude" => 35.01452224262974,
+						"longitude" => 135.673689478396,
+						"transit_time" => "00 :00 :00"
+					],
+					[
+						"latitude" => 35.014693588715424,
+						"longitude" => 135.67381286001063,
+						"transit_time" => "00 :00 :00"
+					],
+					[
+						"latitude" => 35.01448709467034,
+						"longitude" => 135.67387186860896,
+						"transit_time" => "00 :00 :00"
+					],
+					[
+						"latitude" => 35.01429817412983,
+						"longitude" => 135.6738450465188,
+						"transit_time" => "00 :00 :00"
+					],
+					[
+						"latitude" => 35.01407849853396,
+						"longitude" => 135.67394160604334,
+						"transit_time" => "00 :00 :00"
+					]
 				],
 				"spots" => [
 					[
-						"visit_time" => "2016-12-12 10:13:15",
-						"latitude" => 35.668859752941586, 
-						"longitude" => 139.73236638250415,
-						"spot_name" => "ミッドタウン",
-						"site_url" => "http://hoge.com",
-						"stay_minute" => 40,
-						"category_code" => "0304"
+						"visit_time" => "2016-11-26 13:30:15",
+						"latitude" => 35.0115104470532,
+						"longitude" => 135.67740970229926,
+						"spot_name" => "渡月亭",
+						"site_url" => "",
+						"stay_minute" => 5,
+						"category_code" => "0101007"
 					],
 					[
-						"visit_time" => "2016-12-12 10:13:15",
+						"visit_time" => "2016-11-26 13:40:24",
 						"latitude" => 35.66810801257965, 
 						"longitude" => 139.7329717329569,
-						"spot_name" => "ローソン港赤坂九丁目店",
-						"site_url" => "http://hoge.com",
+						"spot_name" => "良弥　本通り店",
+						"site_url" => "",
 						"stay_minute" => 40,
-						"category_code" => "0204"
+						"category_code" => "0101007"
+					],
+					[
+						"visit_time" => "2016-11-26 14:30:41",
+						"latitude" => 35.014712777778, 
+						"longitude" => 135.677769722222,
+						"spot_name" => "いしかわ",
+						"site_url" => "",
+						"stay_minute" => 30,
+						"category_code" => "02"
+					],
+					[
+						"visit_time" => "2016-11-26 15:01:12",
+						"latitude" => 35.015151388889, 
+						"longitude" => 135.675400277778,
+						"spot_name" => "等観院",
+						"site_url" => "",
+						"stay_minute" => 20,
+						"category_code" => "0424001"
+					],
+					[
+						"visit_time" => "2016-11-26 15:01:12",
+						"latitude" => 35.015151388889, 
+						"longitude" => 135.675400277778,
+						"spot_name" => "等観院",
+						"site_url" => "",
+						"stay_minute" => 20,
+						"category_code" => "0424001"
 					]
-				],
+				],				
 			],
 			[
 				"travel_code" => "abcdefg",
